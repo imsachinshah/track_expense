1e71b2a8183b80b5a0c3bccedbe16fa515618b1d
Sachin Shah <sachinshah1999@gmail.com>
2022-10-04 23:39:31 +0530

    fixed something

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -15,52 +15,51 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 362,
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: transactions.map((tx) {
-            return Card(
-              elevation: 5,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 15),
-                    padding: const EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(0),
-                      border: Border.all(
-                        width: 2,
-                        color: Colors.red,
-                      ),
-                    ),
-                    child: Text(
-                      '\$${tx.price}',
-                      style: const TextStyle(color: Colors.red, fontSize: 20),
+      child: ListView.builder(
+        itemCount: transactions.length,
+        itemBuilder: ((context, index) {
+          return Card(
+            elevation: 5,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(0),
+                    border: Border.all(
+                      width: 2,
+                      color: Colors.red,
                     ),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        tx.name,
-                        style: const TextStyle(
-                            fontSize: 17, fontWeight: FontWeight.w400),
-                      ),
-                      Text(
-                        DateFormat('dd/MM/yy').format(tx.date),
-                        style: const TextStyle(color: Colors.grey),
-                      ),
-                    ],
+                  child: Text(
+                    '\$${transactions[index].price}',
+                    style: const TextStyle(color: Colors.red, fontSize: 20),
                   ),
-                ],
-              ),
-            );
-          }).toList(),
-        ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      transactions[index].name,
+                      style: const TextStyle(
+                          fontSize: 17, fontWeight: FontWeight.w400),
+                    ),
+                    Text(
+                      DateFormat('dd/MM/yy').format(transactions[index].date),
+                      style: const TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        } ),
+        
       ),
     );
   }
